b6cdd8d125ea8d6de5885ecf97bf308feff12e9c
4aykovski <kan89kan@gmail.com>
2024-09-01 05:20:53 +0300

    upd

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -272,25 +272,21 @@ M.telescope = {
     -- find
     ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
-    ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
-    ["<leader>fs"] = { "<cmd> Telescope grep_string <CR>", "Grep string" },
+    ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
     ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
     ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
     ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
-    ["<leader>fc"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
+    ["<leader>fz"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
+
+    -- git
+    ["<leader>cm"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
+    ["<leader>gt"] = { "<cmd> Telescope git_status <CR>", "Git status" },
 
     -- pick a hidden term
     ["<leader>pt"] = { "<cmd> Telescope terms <CR>", "Pick hidden term" },
 
     -- theme switcher
     ["<leader>th"] = { "<cmd> Telescope themes <CR>", "Nvchad themes" },
-
-    -- lsp
-    ["<leader>tlr"] = { "<cmd> Telescope lsp_references <CR>", "Find references" },
-    ["<leader>tli"] = { "<cmd> Telescope lsp_implementations <CR>", "Find implementations" },
-    ["<leader>tlt"] = { "<cmd> Telescope lsp_type_definitions <CR>", "Find type definitions" },
-    ["<leader>tgc"] = { "<cmd> Telescope git_commits <CR>", "Show git commits" },
-    ["<leader>tgs"] = { "<cmd> Telescope git_status <CR>", "Show git status" },
 
     ["<leader>ma"] = { "<cmd> Telescope marks <CR>", "telescope bookmarks" },
   },
