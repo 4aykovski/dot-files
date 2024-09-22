e2a06e5ea7600d5dd2c368566184d936059f98a5
4aykovski <kan89kan@gmail.com>
2024-09-22 06:44:39 +0300

    nvim

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -14,3 +14,8 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "window left" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "window down" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "window up" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "window right" })
+
+vim.keymap.set({ "n", "v" }, "<Up>", "<Nop>", { desc = "disable arrows" })
+vim.keymap.set({ "n", "v" }, "<Down>", "<Nop>", { desc = "disable arrows" })
+vim.keymap.set({ "n", "v" }, "<Left>", "<Nop>", { desc = "disable arrows" })
+vim.keymap.set({ "n", "v" }, "<Right>", "<Nop>", { desc = "disable arrows" })
