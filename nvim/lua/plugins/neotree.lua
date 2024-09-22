7e63fc5b9c0158c73485a6f46c76b93b5dc7ed78
4aykovski <kan89kan@gmail.com>
2024-09-22 07:40:28 +0300

    nvim

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -7,6 +7,16 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
+
 		vim.keymap.set("n", "<C-n>", ":Neotree close<CR>", { desc = "close tree" })
 		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", { desc = "open tree" })
 	end,
