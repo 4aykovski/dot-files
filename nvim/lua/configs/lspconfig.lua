a12a8c1bee1ece229246dc09a7d06c98e3331c16
4aykovski <kan89kan@gmail.com>
2024-10-06 19:52:20 +0300

    upd

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -15,6 +15,15 @@ lspconfig.gopls.setup({
 			analyses = {
 				unusedparams = true,
 			},
+			hints = {
+				rangeVariableTypes = true,
+				parameterNames = true,
+				constantValues = true,
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				functionTypeParameters = true,
+			},
 		},
 	},
 })
