d4816dc79048a6f4aa6a6ecb6b290d71737da165
4aykovski <kan89kan@gmail.com>
2024-12-01 19:12:04 +0300

    relative path

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -33,7 +33,7 @@ return {
 				-- Key bindings for managing completions in virtual text mode.
 				key_bindings = {
 					-- Accept the current completion.
-					accept = "<c-Tab>",
+					accept = "<C-c>",
 					-- Accept the next word.
 					accept_word = false,
 					-- Accept the next line.
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -33,7 +33,7 @@ return {
 				sections = {
 					lualine_a = { "mode" },
 					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename", harpoon },
+					lualine_c = { { "filename", path = 1 }, harpoon },
 					lualine_x = { "encoding", "fileformat", "filetype" },
 					lualine_y = { "progress" },
 					lualine_z = { "location" },
