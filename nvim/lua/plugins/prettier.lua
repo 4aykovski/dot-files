81bfd7612a62dc22e10ab715813dbdfc569d77a0
4aykovski <kan89kan@gmail.com>
2025-02-28 17:47:01 +0300

    prettier

--- nvim/lua/plugins/prettier.lua
+++ nvim/lua/plugins/prettier.lua
@@ -15,6 +15,19 @@ return {
 				"typescript",
 				"typescriptreact",
 			},
+			["none-ls"] = {
+				condition = function()
+					return prettier.config_exists({
+						-- if `false`, skips checking `package.json` for `"prettier"` key
+						check_package_json = true,
+					})
+				end,
+				runtime_condition = function(params)
+					-- return false to skip running prettier
+					return true
+				end,
+				timeout = 5000,
+			},
 		})
 	end,
 }
