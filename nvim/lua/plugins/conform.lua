649682eb6c1f6babf84096a60f7f56a45602cc11
ysandyrev <ysandyrev@ozon.ru>
2026-07-10 05:02:46 +0300

    update

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,32 +1,33 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	-- Everything in opts will be passed to setup()
-	opts = {
-		-- Define your formatters
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			javascript = { "prettierd" },
-			typescript = { "prettierd" },
-			go = { "gofmt",  "goimports" },
-			html = { "prettierd" },
-			scss = { "prettierd" },
-			css = { "prettierd" },
-		},
-		-- Set up format-on-save
-		format_on_save = { timeout_ms = 100, lsp_fallback = true },
-		-- Customize formatters
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "2" },
-			},
-		},
-	},
-	init = function()
-		-- If you want the formatexpr, here is the place to set it
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      vue = { "prettier" },
+      go = { "gofmt", "goimports" },
+      html = { "prettier" },
+      scss = { "prettier" },
+      css = { "prettier" },
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 100, lsp_fallback = true },
+    log_level = vim.log.levels.DEBUG,
+    -- Customize formatters
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
-
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,58 +1,83 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"andrew-george/telescope-themes",
-		},
-		config = function()
-			local telescope = require("telescope")
-			local builtin = require("telescope.builtin")
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "andrew-george/telescope-themes",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+      local actions = require("telescope.actions")
 
-			telescope.load_extension("themes")
+      telescope.setup({
+        defaults = {
+          -- Включить перенос строк в окне результатов
+          wrap_results = true,
 
-			-- find
-			vim.keymap.set("n", "<C-f>", builtin.find_files, { desc = "find files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "live grep" })
-			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "find old" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "find buffs" })
-			vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, { desc = "fuzzy find" })
-			vim.keymap.set(
-				"n",
-				"<leader>fa",
-				"<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>",
-				{ desc = "find all" }
-			)
+          -- Настройка отображения путей: интеллектуальное отображение
+          path_display = { "smart" },
 
-			-- jumps
-			vim.keymap.set("n", "<leader>tj", builtin.jumplist, { desc = "jumplist" })
-			vim.keymap.set("n", "<leader>tm", "<cmd> Telescope harpoon marks <CR>", { desc = "harpoon marks" })
+          -- Настройки layout
+          layout_config = {
+            width = 0.9,
+            height = 0.9,
+            preview_cutoff = 120,
+          },
 
-			-- git
-			vim.keymap.set("n", "<leader>tgs", builtin.git_status, { desc = "git status" })
-			vim.keymap.set("n", "<leader>tgc", builtin.git_commits, { desc = "git commits" })
+          -- Клавиши для вертикальной прокрутки
+          mappings = {
+            i = {
+              ["<C-u>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.preview_scrolling_down,
+            },
+            n = {
+              ["<C-u>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.preview_scrolling_down,
+            },
+          },
+        },
+        extensions = {
+          ["ui-select"] = require("telescope.themes").get_dropdown({}),
+        },
+      })
 
-			-- lsp
-			vim.keymap.set("n", "<leader>tli", builtin.lsp_implementations, { desc = "implementations" })
-			vim.keymap.set("n", "<leader>tlr", builtin.lsp_references, { desc = "references" })
-			vim.keymap.set("n", "<leader>tld", builtin.lsp_definitions, { desc = "defenitions" })
+      telescope.load_extension("themes")
+      telescope.load_extension("ui-select")
 
-			-- themes
-			vim.keymap.set("n", "<leader>tt", "<cmd> Telescope themes <CR>", { desc = "themes" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      -- find
+      vim.keymap.set("n", "<C-f>", builtin.find_files, { desc = "find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "live grep" })
+      vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "find old" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "find buffs" })
+      vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, { desc = "fuzzy find" })
+      vim.keymap.set(
+        "n",
+        "<leader>fa",
+        "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>",
+        { desc = "find all" }
+      )
+
+      -- jumps
+      vim.keymap.set("n", "<leader>tj", builtin.jumplist, { desc = "jumplist" })
+      vim.keymap.set("n", "<leader>tm", "<cmd> Telescope harpoon marks <CR>", { desc = "harpoon marks" })
+
+      -- git
+      vim.keymap.set("n", "<leader>tgs", builtin.git_status, { desc = "git status" })
+      vim.keymap.set("n", "<leader>tgc", builtin.git_commits, { desc = "git commits" })
+
+      -- lsp
+      vim.keymap.set("n", "<leader>tli", builtin.lsp_implementations, { desc = "implementations" })
+      vim.keymap.set("n", "<leader>tlr", builtin.lsp_references, { desc = "references" })
+      vim.keymap.set("n", "<leader>tld", builtin.lsp_definitions, { desc = "defenitions" })
+
+      -- themes
+      vim.keymap.set("n", "<leader>tt", "<cmd> Telescope themes <CR>", { desc = "themes" })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    -- Конфигурация теперь в основном setup()
+  },
 }
